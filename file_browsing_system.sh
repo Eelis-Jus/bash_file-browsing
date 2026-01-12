2468f297fc153c1a3482ec913c7ff5423a317203
Eelis-Jus <Eelis.jussila@gmail.com>
2026-01-12 14:48:16 +0200

    -

--- file_browsing_system.sh
+++ file_browsing_system.sh
@@ -1,12 +1,21 @@
 GREEN=$(tput setaf 1)
 #use setaf to use colors
+#25,51,0
 list_files=$(ls -1)
-tput setaf 2; ls -1
-echo ": "
-read user_input
+tput setaf 193; 
+tput setab 22;
+clear;
+ls -1;
 while [ "$user_input" != "exit_fallout" ]; do
-	echo ": " 
-	read user_input
-	eval "$user_input"
-done
+ 	read user_input; 
+	clear;
+	echo "----------------------------------------------------------------------------------------------------";
+	eval "$user_input";
+	command="$user_input" 
+	if [[ "$command" =~ "cd " ]]; then
+		ls -1;
+	fi;
+done;
 tput setaf 0;
+tput sgr0;
+
